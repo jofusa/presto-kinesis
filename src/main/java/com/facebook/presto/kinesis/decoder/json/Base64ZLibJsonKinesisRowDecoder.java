6b868579107787c31c9f9ddc46a8ec68496d1785
John Dennison <dennison.john@gmail.com>
2015-08-08 13:13:04 -0600

    new row type decoder

---
+++ src/main/java/com/facebook/presto/kinesis/decoder/json/Base64ZLibJsonKinesisRowDecoder.java
@@ -0,0 +1,122 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.kinesis.decoder.json;
+
+import com.facebook.presto.kinesis.KinesisColumnHandle;
+import com.facebook.presto.kinesis.KinesisFieldValueProvider;
+import com.facebook.presto.kinesis.decoder.KinesisFieldDecoder;
+import com.facebook.presto.kinesis.decoder.KinesisRowDecoder;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.MissingNode;
+import com.google.common.base.Splitter;
+import com.google.common.io.BaseEncoding;
+import com.google.common.io.ByteStreams;
+import com.google.inject.Inject;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.InflaterInputStream;
+
+import static com.google.common.base.Preconditions.checkState;
+
+public class Base64ZLibJsonKinesisRowDecoder
+        implements KinesisRowDecoder
+{
+    public static final String NAME = "base64-zlib-json";
+
+    private final ObjectMapper objectMapper;
+
+    @Inject
+    public Base64ZLibJsonKinesisRowDecoder(ObjectMapper objectMapper)
+    {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+    @Override
+    public boolean decodeRow(byte[] data, Set<KinesisFieldValueProvider> fieldValueProviders, List<KinesisColumnHandle> columnHandles, Map<KinesisColumnHandle, KinesisFieldDecoder<?>> fieldDecoders)
+    {
+        JsonNode tree;
+
+        String decoded_data = null;
+        try {
+            decoded_data = new String(decodeAndUncompress(data), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+        try {
+            tree = objectMapper.readTree(decoded_data);
+        }
+        catch (Exception e) {
+            return false;
+        }
+
+        for (KinesisColumnHandle columnHandle : columnHandles) {
+            if (columnHandle.isInternal()) {
+                continue;
+            }
+            @SuppressWarnings("unchecked")
+            KinesisFieldDecoder<JsonNode> decoder = (KinesisFieldDecoder<JsonNode>) fieldDecoders.get(columnHandle);
+
+            if (decoder != null) {
+                JsonNode node = locateNode(tree, columnHandle);
+                fieldValueProviders.add(decoder.decode(node, columnHandle));
+            }
+        }
+        return true;
+    }
+
+    private static JsonNode locateNode(JsonNode tree, KinesisColumnHandle columnHandle)
+    {
+        String mapping = columnHandle.getMapping();
+        checkState(mapping != null, "No mapping for %s", columnHandle.getName());
+
+        JsonNode currentNode = tree;
+        for (String pathElement : Splitter.on('/').omitEmptyStrings().split(mapping)) {
+            if (!currentNode.has(pathElement)) {
+                return MissingNode.getInstance();
+            }
+            currentNode = currentNode.path(pathElement);
+        }
+        return currentNode;
+    }
+
+
+    /**
+     * Decodes and decompressed a string of information that's encoded via base 64 and compressed via zlib
+     */
+    public static byte[] decodeAndUncompress(byte[] data) throws IOException {
+
+        String string_data = new String(data,"UTF-8");
+        BaseEncoding decoder = BaseEncoding.base64().withSeparator("\n", 1);
+        ByteArrayInputStream compressed = new ByteArrayInputStream(decoder.decode(string_data));
+        InflaterInputStream decompressed = new InflaterInputStream(compressed);
+
+        return ByteStreams.toByteArray(decompressed);
+    }
+
+}
---
+++ src/test/java/com/facebook/presto/kinesis/decoder/json/TestBase64ZLibJsonDecoder.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.kinesis.decoder.json;
+
+import com.facebook.presto.kinesis.KinesisColumnHandle;
+import com.facebook.presto.kinesis.KinesisFieldValueProvider;
+import com.facebook.presto.kinesis.decoder.KinesisFieldDecoder;
+import com.facebook.presto.spi.type.BigintType;
+import com.facebook.presto.spi.type.BooleanType;
+import com.facebook.presto.spi.type.VarcharType;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.ByteStreams;
+import io.airlift.json.ObjectMapperProvider;
+import org.testng.annotations.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.facebook.presto.kinesis.decoder.util.DecoderTestUtil.checkValue;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class TestBase64ZLibJsonDecoder
+{
+    private static final JsonKinesisFieldDecoder DEFAULT_FIELD_DECODER = new JsonKinesisFieldDecoder();
+    private static final ObjectMapperProvider PROVIDER = new ObjectMapperProvider();
+
+    private static Map<KinesisColumnHandle, KinesisFieldDecoder<?>> buildMap(List<KinesisColumnHandle> columns)
+    {
+        ImmutableMap.Builder<KinesisColumnHandle, KinesisFieldDecoder<?>> map = ImmutableMap.builder();
+        for (KinesisColumnHandle column : columns) {
+            map.put(column, DEFAULT_FIELD_DECODER);
+        }
+        return map.build();
+    }
+
+    @Test
+    public void testSimple()
+            throws Exception
+    {
+        byte[] raw_data = ByteStreams.toByteArray(TestBase64ZLibJsonDecoder.class.getResourceAsStream("/decoder/json/base64_zlib_message.data"));
+
+        Base64ZLibJsonKinesisRowDecoder rowDecoder = new Base64ZLibJsonKinesisRowDecoder(PROVIDER.get());
+
+
+        byte[] decompressed_data = rowDecoder.decodeAndUncompress(raw_data);
+        String string_data = new String(decompressed_data, "UTF-8");
+
+        KinesisColumnHandle row1 = new KinesisColumnHandle("", 0, "row1", VarcharType.VARCHAR, "source", null, null, false, false);
+        KinesisColumnHandle row2 = new KinesisColumnHandle("", 1, "row2", VarcharType.VARCHAR, "user/screen_name", null, null, false, false);
+        KinesisColumnHandle row3 = new KinesisColumnHandle("", 2, "row3", BigintType.BIGINT, "id", null, null, false, false);
+        KinesisColumnHandle row4 = new KinesisColumnHandle("", 3, "row4", BigintType.BIGINT, "user/statuses_count", null, null, false, false);
+        KinesisColumnHandle row5 = new KinesisColumnHandle("", 4, "row5", BooleanType.BOOLEAN, "user/geo_enabled", null, null, false, false);
+
+        List<KinesisColumnHandle> columns = ImmutableList.of(row1, row2, row3, row4, row5);
+        Set<KinesisFieldValueProvider> providers = new HashSet<>();
+
+        boolean valid = rowDecoder.decodeRow(raw_data, providers, columns, buildMap(columns));
+        assertTrue(valid);
+
+        assertEquals(providers.size(), columns.size());
+
+        checkValue(providers, row1, "<a href=\"http://twitterfeed.com\" rel=\"nofollow\">twitterfeed</a>");
+        checkValue(providers, row2, "EKentuckyNews");
+        checkValue(providers, row3, 493857959588286460L);
+        checkValue(providers, row4, 7630);
+        checkValue(providers, row5, true);
+    }
+}
